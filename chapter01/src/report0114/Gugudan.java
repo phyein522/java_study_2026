d073923e324b13887821d7e7f090ccbce0f38a33
phyein522 <phyein522@gmail.com>
2026-01-15 09:21:52 +0900

    report 0114

--- chapter01/src/report0114/Gugudan.java
+++ chapter01/src/report0114/Gugudan.java
@@ -22,5 +22,25 @@ public class Gugudan {
             }
             System.out.println();
         }
+        System.out.println("--------------------------------------------------\n");
+        for (int digit = 2; digit < 9; digit += 3) {
+            for (int n = 0; n < 10; n++) {
+                System.out.println(
+                    (
+                        (n == 0)
+                            ? String.format("%d단 출력 \t\t%d단 출력 \t\t%s%s", digit, digit + 1, (digit+2<10 ? digit+2+"" : ""), (digit+2<10 ? "단 출력" : ""))    // if 1번째 줄
+                            : ((digit < 8) ? String.format("%d * %d = %d \t\t%d * %d = %d \t\t%d * %d = %d" // else 2번째 줄, if 8단 미만
+                                , digit, n, digit * n   // d단
+                                , digit + 1, n, (digit + 1) * n // d+1단
+                                , digit + 2, n, (digit + 2) * n)    //d+2단
+                            : String.format("%d * %d = %d \t\t%d * %d = %d" // else 8단 이상
+                                , digit, n, digit * n   // d단
+                                , digit + 1, n, (digit + 1) * n))   // d+1단
+                        )
+                );
+            }   // for (int n = 0; n < 10; n++)
+            System.out.println();
+        }   // for (int digit = 2; digit < 9; digit += 3)
+
     }
 }
