fcd1bc6d4df7eaca8cb88f036111b9d8643590a3
phyein522 <phyein522@gmail.com>
2026-01-20 15:13:02 +0900

    chapter05 - report0120_2 (String)

--- chapter05/src/main/java/com/phi/Chapter05.java
+++ chapter05/src/main/java/com/phi/Chapter05.java
@@ -46,4 +46,152 @@ public class Chapter05 {
 		kind1 = null;
 		System.out.println("kind2: " + kind2);
 	}
+
+	public void hashCodeExample() {
+		System.out.println("\nhashCode()");
+		String a = "abc";
+		String b = "abc";
+		System.out.println("a:" + a.hashCode() + ", b: " + b.hashCode());
+		b = new String("abc");
+		System.out.println("a:" + a.hashCode() + ", b: " + b.hashCode());
+	}
+	public void equalsExample() {
+		System.out.println("\nEqualsExample");
+
+		String strVar1 = "홍길동";
+		String strVar2 = "홍길동";
+
+		if(strVar1 == strVar2) {
+			System.out.println("strVar1(" + strVar1 + ")과 strVar2(" + strVar2 + ")는 참조가 같음 (strVar1 == strVar2: " + (strVar1 == strVar2) + ")");
+		} else {
+			System.out.println("strVar1(" + strVar1 + ")과 strVar2(" + strVar2 + ")는 참조가 다름 (strVar1 == strVar2: " + (strVar1 == strVar2) + ")");
+		}
+
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1과 strVar2는 문자열이 같음 (strVar1.equals(strVar2): " + strVar1.equals(strVar2) + ")");
+		}
+
+		String strVar3 = new String("홍길동");
+		String strVar4 = new String("홍길동");
+
+		if(strVar3 == strVar4) {
+			System.out.println("strVar3(" + strVar3 + ")과 strVar4(" + strVar4 + ")는 참조가 같음 (strVar3 == strVar4: " + (strVar3 == strVar4) + ")");
+		} else {
+			System.out.println("strVar3(" + strVar3 + ")과 strVar4(" + strVar4 + ")는 참조가 다름 (strVar3 == strVar4: " + (strVar3 == strVar4) + ")");
+		}
+
+		if(strVar3.equals(strVar4)) {
+			System.out.println("strVar1과 strVar2는 문자열이 같음 (strVar3.equals(strVar4): " + strVar3.equals(strVar4) + ")");
+		}
+	}
+	public void emptyStringExample() {
+		System.out.println("\nEmptyStringExample");
+
+		String hobby = "";
+		if(hobby.equals("")) {
+			System.out.println("hobby 변수가 참조하는 String 객체는 빈 문자열 (hobby.equals(\"\"): " + hobby.equals("") + ")");
+		}
+	}
+	public void charAtExample() {
+		System.out.println("\nChatAtExample");
+
+		String ssn = "9506241230123";
+		System.out.println("주민등록번호: " + ssn);
+		char sex = ssn.charAt(6);
+		switch(sex) {
+			case '1':
+			case '3':
+				System.out.println(sex + ": 남자입니다.");
+				break;
+			case '2':
+			case '4':
+				System.out.println(sex + ": 여자입니다.");
+				break;
+		}
+	}
+	public void lengthExample() {
+		System.out.println("\nLengthExample");
+
+		String ssn = "9506241230123";
+		System.out.println("주민등록번호: " + ssn);
+		int length = ssn.length();
+		if(length == 13) {
+			System.out.println("주민등록번호 자릿수가 맞습니다. (" + length + "자리)");
+		} else {
+			System.out.println("주민등록번호 자릿수가 틀립니다. (" + length + "자리)");
+		}
+	}
+	public void replaceExample() {
+		System.out.println("\nReplaceExample");
+
+		String oldStr = "자바 문자열은 불변입니다. 자바 문자열은 String입니다.";
+		System.out.println("oldStr: " + oldStr);
+
+		String newStr = oldStr.replace("자바", "JAVA");
+		System.out.println("newStr(oldStr.replace(\"자바\", \"JAVA\")): " + newStr);
+	}
+	public void subStringExample() {
+		System.out.println("\nSubStringExample");
+
+		String ssn = "880815-1234567";
+		System.out.println("주민등록번호: " + ssn);
+
+		String firstNum = ssn.substring(0, 6);
+		System.out.println("앞 자리 (ssn.substring(0, 6)): " + firstNum);
+
+		String secondNum = ssn.substring(7);
+		System.out.println("뒷 자리 (ssn.substring(7)): " + secondNum);
+	}
+	public void indexOfContainsExample() {
+		System.out.println("\nIndexOfContainsExample");
+
+		String subject = "자바 프로그래밍";
+		System.out.println("문자열: " + subject);
+
+		int location = subject.indexOf("프로그래밍");
+		System.out.println("\"프로그래밍\" 시작 인덱스: " + location);
+
+		String substring = subject.substring(location);
+		System.out.println(subject + ".substring(" + location + "): " + substring);
+
+		location = subject.indexOf("자바");
+		if(location != -1) {
+			System.out.println("자바와 관련된 책이군요. (\"자바\" 시작 인덱스: " + location + ")");
+		} else {
+			System.out.println("자바와 관련 없는 책이군요.");
+		}
+
+		boolean result = subject.contains("자바");
+		if(result) {
+			System.out.println("자바와 관련된 책이군요. (\"" + subject + "\".contains(\"자바\"): " + result + ")");
+		} else {
+			System.out.println("자바와 관련 없는 책이군요. (\"" + subject + "\".contains(\"자바\"): " + result + ")");
+		}
+	}
+	public void splitExample() {
+		System.out.println("\nSplitExample");
+
+		String board = "1,자바 학습,참조 타입 String을 학습합니다.,홍길동";
+
+		String[] tokens = board.split(",");
+
+		System.out.println("번호: " + tokens[0]);
+		System.out.println("제목: " + tokens[1]);
+		System.out.println("내용: " + tokens[2]);
+		System.out.println("성명: " + tokens[3]);
+		System.out.println();
+
+		String[] titles = {"번호", "제목", "내용", "성명"};
+
+		for(int i = 0; i < tokens.length; i++) {
+			System.out.println(titles[i] + ": " + tokens[i]);
+		}
+		System.out.println();
+
+		int i = 0;
+		for(String token : tokens) {
+			System.out.println(titles[i] + ": " + token);
+			i++;
+		}
+	}
 }
--- chapter05/src/main/java/com/phi/Main.java
+++ chapter05/src/main/java/com/phi/Main.java
@@ -3,8 +3,19 @@ package com.phi;
 public class Main {
 	public static void main(String[] args) {
         Chapter05 ch5 = new Chapter05();
+
 		ch5.referenceVariableCompareExample();
 		ch5.nullPointerExceptionExample();
 		ch5.garbageObjectExample();
+
+		ch5.hashCodeExample();
+		ch5.equalsExample();
+		ch5.emptyStringExample();
+		ch5.charAtExample();
+		ch5.lengthExample();
+		ch5.replaceExample();
+		ch5.subStringExample();
+		ch5.indexOfContainsExample();
+		ch5.splitExample();
 	}
 }
