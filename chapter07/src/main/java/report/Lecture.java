936744693bb7ded51c4ea6495f1bfb2bec0aa503
phyein522 <phyein522@gmail.com>
2026-01-28 08:45:41 +0900

    report0127

---
+++ chapter07/src/main/java/report/Lecture.java
@@ -0,0 +1,165 @@
+package report;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Scanner;
+
+@Getter
+@ToString
+public class Lecture {
+	//lecture (과목) {과목명, 학점, 성적, 수강일(년도-학기), 구분 divide(전공 major/교양 refinement)}
+	private String subject;
+	private int credit;
+	private int grade;
+	private String takeDate;
+	private String divide;
+
+	private final static int A = 5;
+	private final static int B = 4;
+	private final static int C = 3;
+	private final static int D = 2;
+	private final static int E = 1;
+	private final static int F = 0;
+
+	public Lecture() {
+		newLecture();
+		System.out.println(this.toString());
+	}
+
+//	private Lecture(String subject, int credit, int grade, String takeDate, String divide) {
+//		this.subject = subject;
+//		this.credit = credit;
+//		this.grade = grade;
+//		this.takeDate = takeDate;
+//		this.divide = divide;
+//	}
+
+	private void setSubject(String subject) { this.subject = subject; }
+	private void setCredit(int credit) { this.credit = credit; }
+	private void setGrade(int grade) { this.grade = grade; }
+	private void setTakeDate(String takeDate) { this.takeDate = takeDate; }
+	private void setDivide(String divide) { this.divide = divide; }
+
+	private void newLecture() {
+		Scanner scanner = new Scanner(System.in);
+
+//		System.out.print("\n과목명 (1~100자): ");
+//		String subject = scanner.nextLine();
+//		if(!isValid(subject, "subject")) { return null; }
+		String subject;
+		do {
+			System.out.print("\n과목명 (1~100자): ");
+			subject = scanner.nextLine();
+		} while(!isValid(subject, "subject"));
+		this.setSubject(subject);
+
+//		System.out.print("\n학점: ");
+//		String creditString = scanner.nextLine();
+//		if(!isValid(creditString, "credit")) { return null; }
+//		int credit = Integer.parseInt(creditString);
+		String credit;
+		do {
+			System.out.print("\n학점: ");
+			credit = scanner.nextLine();
+		} while(!isValid(credit, "credit"));
+		this.setCredit(Integer.parseInt(credit));
+
+//		System.out.print("\n성적 (A~F): ");
+//		String gradeString = scanner.nextLine();
+//		if(!isValid(gradeString, "grade")) { return null; }
+//		int grade = switch(gradeString) {
+//			case "A", "a" -> A;
+//			case "B", "b" -> B;
+//			case "C", "c" -> C;
+//			case "D", "d" -> D;
+//			case "E", "e" -> E;
+//			default -> F;
+//		};
+		String grade;
+		do {
+			System.out.print("\n성적 (A~F): ");
+			grade = scanner.nextLine();
+		} while(!isValid(grade, "grade"));
+		this.setGrade(switch(grade) {
+			case "A", "a" -> A;
+			case "B", "b" -> B;
+			case "C", "c" -> C;
+			case "D", "d" -> D;
+			case "E", "e" -> E;
+			default -> F;
+		});
+
+//		System.out.print("\n수강일 (년도-학기, 예: 2000-3): ");
+//		String takeDate = scanner.nextLine();
+//		if(!isValid(takeDate, "takeDate")) { return null; }
+		String takeDate;
+		do {
+			System.out.print("\n수강일 (년도-학기, 예: 2000-3): ");
+			takeDate = scanner.nextLine();
+		} while(!isValid(takeDate, "takeDate"));
+		this.setTakeDate(takeDate);
+
+//		System.out.print("\n구분 (전공(major)/교양(refinement): ");
+//		String divide = scanner.nextLine();
+//		if(!isValid(divide, "divide")) { return null; }
+		String divide;
+		do {
+			System.out.print("\n구분 (전공(major)/교양(refinement): ");
+			divide = scanner.nextLine();
+		} while(!isValid(divide, "divide"));
+		this.setDivide(switch(divide) {
+			case "전공", "major", "Major", "MAJOR", "교양" -> "MAJOR";
+			default -> "REFINEMENT";
+		});
+
+//		Lecture lecture = new Lecture(subject, credit, grade, takeDate, divide);
+//		return lecture;
+	}
+
+	private static boolean isValid(String input, String check) {
+		switch(check) {
+			case "subject":
+				if(input == null || input.isBlank() || input.length() > 100) {
+					System.out.println("과목명을 1~100글자 범위로 입력하세요.");
+					return false;
+				}
+				break;
+			case "credit":
+				if(!input.matches("[0-9]{1,3}")) {
+					System.out.println("학점을 0~100 범위로 입력하세요.");
+					return false;
+				}
+				break;
+			case "grade":
+				if(!input.matches("[A-Fa-f]{1}")) {
+					System.out.println("성적을 A~F 범위로 입력하세요.");
+					return false;
+				}
+				break;
+			case "takeDate":
+				if(!input.matches("[0-9]{4}-[0-9]{1}")) {
+					System.out.println("수강일을 년도-학기 형식(예: 2000-3)으로 입력하세요.");
+					return false;
+				}
+				break;
+			case "divide":
+				switch(input) {
+					case "전공":
+					case "major":
+					case "Major":
+					case "MAJOR":
+					case "교양":
+					case "refinement":
+					case "Refinement":
+					case "REFINEMENT":
+						break;
+					default:
+						System.out.println("구분을 전공/교양 중에서 입력하세요.");
+						return false;
+				}
+				break;
+		}
+		return true;
+	}
+}
---
+++ chapter07/src/main/java/report/Student.java
@@ -0,0 +1,178 @@
+package report;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+@Getter
+@ToString
+public class Student {
+	//student {이름, 학번, 학과, 생년월일, 전화번호, 이메일, 총학점, 평균학점}
+	//takeLecture (수강)
+	private String name;
+	private String major;
+	private int number;
+	private LocalDate birth;
+	private String phone;
+	private String email;
+	private int totalCredit = 0;
+	private double averageGrade = 0;
+	private Lecture[] lectures = new Lecture[0];
+
+	public Student() {
+		newStudent();
+		System.out.println(this.toString());
+	}
+
+//	private Student(String name, String major, int number, LocalDate birth, String phone, String email) {
+//		this.name = name;
+//		this.major = major;
+//		this.number = number;
+//		this.birth = birth;
+//		this.phone = phone;
+//		this.email = email;
+//	}
+
+	public void setTotalCredit(int totalCredit) { this.totalCredit = totalCredit; }
+	public void setAverageGrade(double averageGrade) { this.averageGrade = averageGrade; }
+	private void setName(String name) { this.name = name; }
+	private void setMajor(String major) { this.major = major; }
+	private void setNumber(int number) { this.number = number; }
+	private void setBirth(LocalDate birth) { this.birth = birth; }
+	private void setPhone(String phone) { this.phone = phone; }
+	private void setEmail(String email) { this.email = email; }
+	private void setLectrues(Lecture[] lectures) { this.lectures = lectures; }
+
+	private void newStudent() {
+		Scanner scanner = new Scanner(System.in);
+
+//		System.out.print("\n이름 (1~100자): ");
+//		String name = scanner.nextLine();
+//		if(!isValid(name, "name")) { return null; }
+		String name;
+		do {
+			System.out.print("\n이름 (1~100자): ");
+			name = scanner.nextLine();
+		} while(!isValid(name, "name"));
+		this.setName(name);
+
+//		System.out.print("\n학과 (1~100자): ");
+//		String major = scanner.nextLine();
+//		if(!isValid(major, "major")) { return null; }
+		String major;
+		do {
+			System.out.print("\n학과 (1~100자): ");
+			major = scanner.nextLine();
+		} while(!isValid(major, "major"));
+		this.setMajor(major);
+
+//		System.out.print("\n학번 (10자리 숫자): ");
+//		String numberString = scanner.nextLine();
+//		if(!isValid(numberString, "number")) { return null; }
+//		int number = Integer.parseInt(numberString);
+		String number;
+		do {
+			System.out.print("\n학번 (10자리 숫자): ");
+			number = scanner.nextLine();
+		} while(!isValid(number, "number"));
+		this.setNumber(Integer.parseInt(number));
+
+//		System.out.print("\n생년월일 (yyyy-MM-dd) (선택): ");
+//		String birthString = scanner.nextLine();
+//		if(!birthString.isBlank() && !isValid(birthString, "birth")) { return null; }
+//		LocalDate birth = birthString.isBlank() ? null : LocalDate.parse(birthString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		String birth;
+		do {
+			System.out.print("\n생년월일 (yyyy-MM-dd) (선택): ");
+			birth = scanner.nextLine();
+		} while(!birth.isBlank() && !isValid(birth, "birth"));
+		this.setBirth(birth.isBlank() ? null : LocalDate.parse(birth, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+//		System.out.print("\n전화번호 (010-0000-0000) (선택): ");
+//		String phone = scanner.nextLine();
+//		if(!phone.isBlank() && !isValid(phone, "phone")) { return null; }
+//		phone = phone.isBlank() ? null : phone;
+		String phone;
+		do {
+			System.out.print("\n전화번호 (010-0000-0000) (선택): ");
+			phone = scanner.nextLine();
+		} while(!phone.isBlank() && !isValid(phone, "phone"));
+		this.setPhone(phone.isBlank() ? null : phone);
+
+//		System.out.print("\n이메일 (선택): ");
+//		String email = scanner.nextLine();
+//		if(!email.isBlank() && !isValid(email, "email")) { return null; }
+//		phone = email.isBlank() ? null : email;
+		String email;
+		do {
+			System.out.print("\n이메일 (선택): ");
+			email = scanner.nextLine();
+		} while(!email.isBlank() && !isValid(email, "email"));
+		this.setEmail(email.isBlank() ? null : email);
+
+//		Student student = new Student(name, major, number, birth, phone, email);
+//		return student;
+	}
+
+	private static boolean isValid(String input, String check) {
+		switch(check) {
+			case "name":
+			case "major":
+				if(input == null || input.isBlank() || input.length() > 100) {
+					System.out.print(check.equals("name") ? "이름을" : "학과를");
+					System.out.println(" 1~100글자 범위로 입력하세요.");
+					return false;
+				}
+				break;
+			case "number":
+				if(!input.matches("[0-9]{10}")) {
+					System.out.println("학번을 10자리 숫자로 입력하세요.");
+					return false;
+				}
+				break;
+			case "birth":
+				try {
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+					LocalDate.parse(input, formatter);
+				} catch (DateTimeParseException e) {
+					System.out.println("날짜를 yyyy-MM-dd 형식으로 입력해주세요.");
+					return false;
+				}
+				break;
+			case "phone":
+				if(!input.matches("010-[0-9]{4}-[0-9]{4}")) {
+					System.out.println("전화번호를 010-0000-0000 형식으로 입력해주세요.");
+					return false;
+				}
+				break;
+			case "email":
+				if(!input.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$")) {
+					System.out.println("이메일을 이메일 형식으로 입력해주세요.");
+					return false;
+				}
+				break;
+		}
+		return true;
+	}
+
+	public void takeLecture() {
+		Lecture newLecture = new Lecture();
+		Lecture[] newLectures = new Lecture[this.getLectures().length + 1];
+		int i;
+		int totalCredit = 0;
+		for(i = 0; i < this.getLectures().length; i++) {
+			newLectures[i] = this.getLectures()[i];
+			totalCredit += newLectures[i].getCredit();
+		}
+		newLectures[i] = newLecture;
+		totalCredit += newLectures[i].getCredit();
+		this.setLectrues(newLectures);
+		this.setTotalCredit(totalCredit);
+		this.setAverageGrade((double)totalCredit/this.getLectures().length);
+		System.out.println(this.toString());
+	}
+}
