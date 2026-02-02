3cceba3d52b5a0d728a245a407c74a9cbabc62bc
phyein522 <phyein522@gmail.com>
2026-02-02 16:28:18 +0900

    report0130 - 2 (상속)

--- chapter08/src/main/java/report0130/report02/Nurse.java
+++ chapter08/src/main/java/report0130/report02/Nurse.java
@@ -24,7 +24,7 @@ public class Nurse {
 		this.counter = counter;
 	}
 
-	public void record(Object animal) {
+	public void record(Animal animal) {
 		if(animal == null) {
 			return;
 		}
@@ -32,26 +32,10 @@ public class Nurse {
 		String name = "";
 		String owner = "";
 		String species = "";
-		if(animal instanceof Cat cat) {
-			name = cat.getName();
-			owner = cat.getOwner();
-			species = cat.getSpecies();
-		} else if(animal instanceof Parrot parrot) {
-			name = parrot.getName();
-			owner = parrot.getOwner();
-			species = parrot.getSpecies();
-		} else if(animal instanceof LargeDog largeDog) {
-			name = largeDog.getName();
-			owner = largeDog.getOwner();
-			species = largeDog.getSpecies();
-		} else if(animal instanceof SmallDog smallDog) {
-			name = smallDog.getName();
-			owner = smallDog.getOwner();
-			species = smallDog.getSpecies();
-		} else if(animal instanceof Rabbit rabbit) {
-			name = rabbit.getName();
-			owner = rabbit.getOwner();
-			species = rabbit.getSpecies();
+		if(animal instanceof Animal a) {
+			name = a.getName();
+			owner = a.getOwner();
+			species = a.getSpecies();
 		}
 		if(containAnimal(name, owner, species)) {
 			System.out.println("해당 환자는 이미 기록됐습니다.");
@@ -101,89 +85,6 @@ public class Nurse {
 
 		AnimalPatient animalPatient = new AnimalPatient(name, owner, species, doctorInCharge, hospitalization, dischargeDate, number);
 		addAnimalPatient(animalPatient);
-
-		/*
-		Scanner scanner = new Scanner(System.in);
-
-		String name;
-		do {
-			System.out.print("환자 이름을 입력하세요:  ");
-			name = scanner.nextLine();
-		} while(name.isBlank());
-		if(name.length() > 100) {
-			name = name.substring(0, 99);
-		}
-
-		String owner;
-		do {
-			System.out.print("주인 이름을 입력하세요:  ");
-			owner = scanner.nextLine();
-		} while(owner.isBlank());
-		if(owner.length() > 100) {
-			owner = owner.substring(0, 99);
-		}
-
-		//고양이, 앵무새, 대형견, 소형견, 토끼
-		String species;
-		do {
-			System.out.print("종을 입력하세요. (입원 가능 종: 고양이(CAT), 앵무새(PARROT), 대형견(LARGE_DOG), 소형견(SMALL_DOG), 토끼(RABBIT)):  ");
-			species = scanner.nextLine();
-			species = switch(species) {
-				case "고양이", "cat", "Cat", "CAT" -> "CAT";
-				case "앵무새", "parrot", "Parrot", "PARROT" -> "PARROT";
-				case "대형견", "대형 견", "largedog", "large dog", "Largedog", "Large dog", "LargeDog", "Large Dog", "LARGEDOG", "LARGE DOG", "large_dog", "Large_dog", "Large_Dog", "LARGE_DOG", "bigdog", "big dog", "Bigdog", "Big dog", "BigDog", "Big Dog", "BIGDOG", "BIG DOG", "big_dog","Big_dog","Big_Dog", "BIG_DOG" -> "LARGE_DOG";
-				case "소형견", "소형 견", "smalldog", "small dog", "Smalldog", "Small dog", "SmallDog", "Small Dog", "SMALLDOG", "SMALL DOG", "small_dog", "Small_dog", "Small_Dog", "SMALL_DOG" -> "SMALL_DOG";
-				case "토끼", "rabbit", "Rabbit", "RABBIT" -> "RABBIT";
-				default -> species;
-			};
-			if(!species.equals("CAT") && !species.equals("PARROT") && !species.equals("LARGE_DOG") && !species.equals("SMALL_DOG") && !species.equals("RABBIT")) {
-				System.out.println("입원 가능 종은 고양이, 앵무새, 대형견, 소형견, 토끼 뿐입니다.");
-				return;
-			}
-		} while(species.isBlank());
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-		String hospitalizationDateString;
-		LocalDate hospitalization = LocalDate.now();
-		do {
-			System.out.print("입원일 (yyyy-MM-dd):  ");
-			hospitalizationDateString = scanner.nextLine();
-			try {
-				hospitalization = LocalDate.parse(hospitalizationDateString, formatter);
-			} catch (DateTimeParseException e) {
-				System.out.println("입원일을 yyyy-MM-dd 형태로 입력하세요.");
-				hospitalizationDateString = "";
-			}
-		} while(hospitalizationDateString.isBlank());
-
-		String dischargeDateString;
-		LocalDate dischargeDate = LocalDate.now();
-		do {
-			System.out.print("퇴원일 (yyyy-MM-dd):  ");
-			dischargeDateString = scanner.nextLine();
-			try {
-				dischargeDate = LocalDate.parse(dischargeDateString, formatter);
-			} catch (DateTimeParseException e) {
-				System.out.println("퇴원일을 yyyy-MM-dd 형태로 입력하세요.");
-				dischargeDateString = "";
-			}
-		} while(dischargeDateString.isBlank());
-
-		String doctorInCharge;
-		do {
-			System.out.print("담당 의사:  ");
-			doctorInCharge = scanner.nextLine();
-		} while(doctorInCharge.isBlank());
-		if(doctorInCharge.length() > 100) {
-			doctorInCharge = doctorInCharge.substring(0, 99);
-		}
-
-		String number = String.valueOf(this.getAnimalPatients().length);
-
-		AnimalPatient animalPatient = new AnimalPatient(name, owner, species, doctorInCharge, hospitalization, dischargeDate, number);
-		addAnimalPatient(animalPatient);
-		*/
 }
 
 	public void addAnimalPatient(AnimalPatient animalPatient) {
@@ -292,34 +193,4 @@ public class Nurse {
 			System.out.printf("식별 번호: %s \t\n", animalPatient.getNumber());
 		}
 	}
-/*
-	public void startWork() {
-		System.out.println("출근합니다.");
-		Scanner scanner = new Scanner(System.in);
-		while(true) {
-			System.out.println("\n\n--------------------------------------------------");
-			System.out.println("1. 환자 기록 | 2. 약 먹이기 | 3. 환자 면회 | 4. 환자 목록 확인 | 5. 퇴근");
-			System.out.println("--------------------------------------------------");
-			System.out.print(">>  ");
-			String select = scanner.nextLine();
-			switch(select) {
-				case "1":
-					record();
-					break;
-				case "2":
-					takeMedicine();
-					break;
-				case "3":
-					visit();
-					break;
-				case "4":
-					printAnimalPatientsList();
-					break;
-				case "5":
-					System.out.println("퇴근합니다.");
-					return;
-			}
-		}
-	}
-*/
 }
